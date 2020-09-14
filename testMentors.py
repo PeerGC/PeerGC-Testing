3ba9227a84051f6afd3dff16ffe6e16d2dd3b414
Artemas J. Radik <ajradik@gmail.com>
2020-09-13 20:44:55 -0400

    Update testMentors.py

--- testMentors.py
+++ testMentors.py
@@ -9,27 +9,25 @@ default_app = firebase_admin.initialize_app()
 NUMBER_OF_USERS = 600
 db = firestore.Client()
 
-# accountType
+# Account_Type
 accountTypes = ["student", "mentor"]
 
-# zipCode
+# ZIP_Code
 zipCodesFile = open("zipCodes.txt", "r")
 zipCodes = zipCodesFile.read().split("\n")
 zipCodesFile.close()
 
-# zipCodeMedianIncome
-
-# photoURL
+# Photo_URL
 photoURLs = ["https://peaklife.in/wp-content/uploads/2019/06/elon-musk-image.jpg",
              "https://observer.com/wp-content/uploads/sites/2/2020/01/elon-musk-twitter-advice.jpg?quality=80&strip",
              "https://i.insider.com/5ddfa893fd9db26b8a4a2df7?width=1100&format=jpeg&auto=webp"]
 
-# firstName
+# First_Name
 firstNamesFile = open("firstNames.txt", "r")
 firstNames = firstNamesFile.read().split("\n")
 firstNamesFile.close()
 
-# state
+# State
 states = ["Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut",
           "Delaware", "District of Columbia", "Florida", "Georgia", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana",
           "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota",
@@ -39,59 +37,58 @@ states = ["Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "Califor
           "Tennessee", "Texas", "U.S. Virgin Islands", "Utah", "Vermont", "Virginia", "Washington", "West Virginia",
           "Wisconsin", "Wyoming"]
 
-# financialLevel
-financialLevels = ["belowAverage", "average", "aboveAverage"]
+# Financial_Level
+financialLevels = ["below_average", "average", "above_average"]
 
-# gender
-genders = ["male", "female", "nonBinary", "other"]
+# Gender
+genders = ["male", "female", "non_binary", "other"]
 
-# lgbtq
+# LGBTQ
 lgbtqs = ["yes", "no"]
 
 # race
-races = ["white", "blackAfricanAmerican", "americanIndianAlaskaNative", "asian", "nativeHawaiianPacificIslander"]
+races = ["white", "black_or_african_american", "american_indian_or_alaska_native", "asian", "native_hawaiian_or_pacific_islander"]
 
-# parentsGoToCollege
-parentsGoToColleges = ["yes", "no", "partially"]
+# Did_Either_Of_Your_Parents_Attend_Higher_Education
+parentsGoToColleges = ["yes", "no", "partially_but_with_no_degree"]
 
-# schoolYear
+# What_Year_Of_College
 schoolYears = ["freshman", "sophomore", "junior", "senior"]
 
-# major
-majors = ["humanities", "mathComputerScience", "sciences", "business", "artTheatre"]
+# What_Field_Of_Study_Are_You_Currently_Pursuing
+majors = ["humanities", "math_or_computer_science", "sciences", "business", "art_or_theatre"]
 
-# collegeName
+# What_College_Do_You_Attend
 collegeNames = ["Columbia", "Stanford", "Carnegie Mellon"]
 
-# highSchoolGPA
-highSchoolGPAs = ["twoOrUnder", "betweenTwoAndThree", "betweenThreeAndFour", "fourPlus"]
+# What_Was_Your_High_School_GPA
+highSchoolGPAs = ["two_or_under", "between_two_and_three", "between_three_and_four", "four_or_higher"]
 
-# testTaken
-testTakens = ["sat", "act", "otherNone"]
+# Which_Test_Did_You_Use_For_Your_College_Application
+testTakens = ["sat", "act", "other_or_none"]
 
-# testScore
+# Test_Score
 testScores = ["36", "1600"]
 
-# helpMost
-helpMosts = ["generalGuidance", "infoCollegeLookFor", "findingSupportSystem", "collegeEntranceTests",
-             "applicationEssays"]
+# Where_Can_You_Help_A_Student_Most
+helpMosts = ["general_guidance_or_keeping_on_track", "info_on_what_colleges_look_for", "finding_a_support_system_in_college", "college_entrance_tests","application_essays"]
 
-# whyYourCollege
-whyYourColleges = ["closeToHome", "bigNameSchool", "bestScholarship", "bestReligionCultureFit", "somethingElse"]
+# Why_Did_You_Choose_The_College_You_Are_In
+whyYourColleges = ["close_to_home", "big_name_school", "best_scholarship", "best_fit_with_your_religion_or_culture", "something_else"]
 
-# postGradAspirations
-postGradAspirationss = ["continuedStudy", "athletics", "relatedIndustry", "earnMoney", "somethingElse"]
+# What_Are_Your_Post_Grad_Aspirations
+postGradAspirationss = ["continued_study", "athletics", "work_in_an_industry_related_to_your_major", "earn_money_with_your_degree", "something_else"]
 
-# whyYouWantBeCounselor
-whyYouWantBeCounselors = ["wishSomethingLikeThisExisted", "canHelpWriteStrongEssays", "scoredWellOnAdmissionsTests",
-                            "sociallyEmotionallySupport", "somethingElse"]
+# Why_Do_You_Want_To_Be_A_Peer_Guidance_Counselor
+whyYouWantBeCounselors = ["you_wish_something_like_this_existed_for_you", "you_can_help_write_strong_essays", "you_scored_well_on_admissions_tests",
+                            "you_can_socially_or_emotionally_support_mentees", "something_else"]
 
-# whichStudentType
-whichStudentTypes = ["financiallyUnderpriveleged", "lgtbq", "womenInStem", "similarCultureReligion",
-                     "similarRacialBackground"]
+# What_Kind_Of_Student_Would_You_Be_Most_Excited_To_Mentor
+whichStudentTypes = ["financially_underprivileged", "lgtbq", "women_in_stem", "similar_cultural_or_religious_background_as_you",
+                     "similar_racial_background_as_you"]
 
-# whichDegree
-whichDegrees = ["aa", "aaForTransfer", "bachelorArtScience", "tradeSchoolDegree", "other"]
+# What_Kind_Of_Degree_Are_You_Currently_Pursuing
+whichDegrees = ["aa", "aa_for_transfer", "bachelor_of_art_or_science", "trade_school_degree", "other"]
 
 # firstLanguage
 firstLanguages = ["English", "Spanish", "Russian"]
@@ -111,31 +108,31 @@ for i in range(NUMBER_OF_USERS):
         continue
 
     data = {
-        u"accountType": random.choice(accountTypes),
-        u"zipCode": random.choice(zipCodes),
-        u"photoURL": random.choice(photoURLs),
-        u"firstName": firstName,
-        u"state": random.choice(states),
-        u"financialLevel": random.choice(financialLevels),
-        u"gender": random.choice(genders),
-        u"lgbtq": random.choice(lgbtqs),
-        u"race": random.choice(races),
-        u"parentsGoToCollege": random.choice(parentsGoToColleges),
-        u"schoolYear": random.choice(schoolYears),
-        u"major": random.choice(majors),
-        u"collegeName": random.choice(collegeNames),
-        u"highSchoolGPA": random.choice(highSchoolGPAs),
-        u"testTaken": random.choice(testTakens),
-        u"testScore": random.choice(testScores),
-        u"helpMost": random.choice(helpMosts),
-        u"whyYourCollege": random.choice(whyYourColleges),
-        u"postGradAspirations": random.choice(postGradAspirationss),
-        u"whyYouWantBeCounselor": random.choice(whyYouWantBeCounselors),
-        u"whichStudentType": random.choice(whichStudentTypes),
-        u"whichDegree": random.choice(whichDegrees),
-        u"firstLanguage": random.choice(firstLanguages)
+        u"Account_Type": random.choice(accountTypes),
+        u"ZIP_Code": random.choice(zipCodes),
+        u"Photo_URL": random.choice(photoURLs),
+        u"First_Name": firstName,
+        u"State": random.choice(states),
+        u"Financial_Level": random.choice(financialLevels),
+        u"Gender": random.choice(genders),
+        u"LGBTQ": random.choice(lgbtqs),
+        u"Race": random.choice(races),
+        u"Did_Either_Of_Your_Parents_Attend_Higher_Education": random.choice(parentsGoToColleges),
+        u"What_Year_Of_College": random.choice(schoolYears),
+        u"What_Field_Of_Study_Are_You_Currently_Pursuing": random.choice(majors),
+        u"What_College_Do_You_Attend": random.choice(collegeNames),
+        u"What_Was_Your_High_School_GPA": random.choice(highSchoolGPAs),
+        u"Which_Test_Did_You_Use_For_Your_College_Application": random.choice(testTakens),
+        u"Test_Score": random.choice(testScores),
+        u"Where_Can_You_Help_A_Student_Most": random.choice(helpMosts),
+        u"Why_Did_You_Choose_The_College_You_Are_In": random.choice(whyYourColleges),
+        u"What_Are_Your_Post_Grad_Aspirations": random.choice(postGradAspirationss),
+        u"What_Are_Your_Post_Grad_Aspirations": random.choice(whyYouWantBeCounselors),
+        u"What_Kind_Of_Student_Would_You_Be_Most_Excited_To_Mentor": random.choice(whichStudentTypes),
+        u"What_Kind_Of_Degree_Are_You_Currently_Pursuing": random.choice(whichDegrees),
+        u"First_Language": random.choice(firstLanguages)
     }
 
-    db.collection(u"users").document(user.uid).set(data)
+    db.collection(u"Users").document(user.uid).set(data)
 
     print("Successfully created new user: {0}".format(user.uid))
